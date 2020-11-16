c7ba3831b6f48fc8da1dcf9d6d9d7ed288b27431
Ivan Matas <ivan.matas2@gmail.com>
2020-11-16 18:49:01 +0100

    Implement admin section

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :organizations
+      resources :cities
+
+      root to: "organizations#index"
+    end
   resources :organizations
 
   root 'organizations#new'
