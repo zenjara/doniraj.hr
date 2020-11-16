07d5fc6087a149c4d9b182e453523488524ec834
Ivan Matas <ivan.matas2@gmail.com>
2020-11-16 18:51:28 +0100

    auto correct with rubocop

--- app/dashboards/city_dashboard.rb
+++ app/dashboards/city_dashboard.rb
@@ -1,4 +1,4 @@
-require "administrate/base_dashboard"
+require 'administrate/base_dashboard'
 
 class CityDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
@@ -12,7 +12,7 @@ class CityDashboard < Administrate::BaseDashboard
     id: Field::Number,
     name: Field::String,
     created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    updated_at: Field::DateTime
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -20,30 +20,16 @@ class CityDashboard < Administrate::BaseDashboard
   #
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
-  COLLECTION_ATTRIBUTES = %i[
-  organizations
-  id
-  name
-  created_at
-  ].freeze
+  COLLECTION_ATTRIBUTES = [:organizations, :id, :name, :created_at].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
-  SHOW_PAGE_ATTRIBUTES = %i[
-  organizations
-  id
-  name
-  created_at
-  updated_at
-  ].freeze
+  SHOW_PAGE_ATTRIBUTES = [:organizations, :id, :name, :created_at, :updated_at].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
-  FORM_ATTRIBUTES = %i[
-  organizations
-  name
-  ].freeze
+  FORM_ATTRIBUTES = [:organizations, :name].freeze
 
   # COLLECTION_FILTERS
   # a hash that defines filters that can be used while searching via the search
--- app/dashboards/organization_dashboard.rb
+++ app/dashboards/organization_dashboard.rb
@@ -1,4 +1,4 @@
-require "administrate/base_dashboard"
+require 'administrate/base_dashboard'
 
 class OrganizationDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
@@ -9,8 +9,8 @@ class OrganizationDashboard < Administrate::BaseDashboard
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     city: Field::BelongsTo.with_options(
-        searchable: true,
-        searchable_fields: ['name']
+      searchable: true,
+      searchable_fields: ['name']
     ),
     id: Field::Number,
     name: Field::String,
@@ -23,7 +23,7 @@ class OrganizationDashboard < Administrate::BaseDashboard
     verified: Field::Boolean,
     archived: Field::Boolean,
     created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    updated_at: Field::DateTime
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -31,49 +31,16 @@ class OrganizationDashboard < Administrate::BaseDashboard
   #
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
-  COLLECTION_ATTRIBUTES = %i[
-  id
-  name
-  address
-  city
-  iban
-  verified
-  archived
-  ].freeze
+  COLLECTION_ATTRIBUTES = [:id, :name, :address, :city, :iban, :verified, :archived].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
-  SHOW_PAGE_ATTRIBUTES = %i[
-  city
-  id
-  name
-  address
-  telephone
-  email
-  iban
-  website
-  swift
-  verified
-  archived
-  created_at
-  updated_at
-  ].freeze
+  SHOW_PAGE_ATTRIBUTES = [:city, :id, :name, :address, :telephone, :email, :iban, :website, :swift, :verified, :archived, :created_at, :updated_at].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
-  FORM_ATTRIBUTES = %i[
-  city
-  name
-  address
-  telephone
-  email
-  iban
-  website
-  swift
-  verified
-  archived
-  ].freeze
+  FORM_ATTRIBUTES = [:city, :name, :address, :telephone, :email, :iban, :website, :swift, :verified, :archived].freeze
 
   # COLLECTION_FILTERS
   # a hash that defines filters that can be used while searching via the search
@@ -86,10 +53,10 @@ class OrganizationDashboard < Administrate::BaseDashboard
   #     open: ->(resources) { resources.where(open: true) }
   #   }.freeze
   COLLECTION_FILTERS = {
-      archived: ->(resources) { resources.archived },
-      verified: ->(resources) { resources.verified },
-      unverified: ->(resources) { resources.unverified },
-      unverified_and_not_archived: ->(resources) { resources.unverified_and_not_archived }
+    archived: ->(resources) { resources.archived },
+    verified: ->(resources) { resources.verified },
+    unverified: ->(resources) { resources.unverified },
+    unverified_and_not_archived: ->(resources) { resources.unverified_and_not_archived }
   }.freeze
 
   # Overwrite this method to customize how organizations are displayed
