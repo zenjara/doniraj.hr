c478dec2d25b5b31b1ee8006bec68d6dd7c06155
Ivan Matas <ivan.matas2@gmail.com>
2020-12-10 21:08:03 +0100

    Implement pagination

--- app/assets/javascripts/controllers/search.js
+++ app/assets/javascripts/controllers/search.js
@@ -2,16 +2,16 @@
     const application = Stimulus.Application.start();
 
     application.register("search", class extends Stimulus.Controller {
-        static targets = ["results", "form", "input", "select"]
+        static targets = ["results", "form", "input", "select", "pagination"]
 
-        connect(){
+        connect() {
             let $citySelect = $("#citySelect")
             $citySelect.on('select2:select', function () {
                 let event = new Event('change')
                 this.dispatchEvent(event);
             });
 
-            $citySelect.on("select2:unselecting", function(e) {
+            $citySelect.on("select2:unselecting", function (e) {
                 let event = new Event('change')
                 this.dispatchEvent(event);
             });
@@ -20,17 +20,57 @@
         search() {
             clearTimeout(this.timeout)
             this.timeout = setTimeout(() => {
-                // if (this.inputTarget.value.length >= 2) {
-                    Rails.fire(this.formTarget, 'submit')
-                // }
+                Rails.fire(this.formTarget, 'submit')
+                Pagy.init(document.getElementById('organizationsList'))
             }, 200)
         }
 
         handleResults() {
             const [data, status, xhr] = event.detail
             if (data.html) {
-                document.getElementById('organizationsList').innerHTML = data.html;
+                this.refreshList(data)
             }
+        }
+
+        paginate(e) {
+            e.preventDefault();
+            this.initiatePaginationRequest(e, e.currentTarget.innerHTML)
+        }
+
+        nextPage(e) {
+            e.preventDefault();
+            let currentPage = parseInt($('span.current').text());
+            let nextPage = currentPage + 1;
+            this.initiatePaginationRequest(e, nextPage)
+        }
+
+        previousPage(e) {
+            e.preventDefault();
+            let currentPage = parseInt($('span.current').text());
+            let previousPage = currentPage - 1;
+
+            this.initiatePaginationRequest(e, previousPage)
+        }
+
+        initiatePaginationRequest(e, page){
+            $.ajax({
+                cache: false,
+                type: 'get',
+                data: {
+                    organization_name: $('#organization_name').val(),
+                    city_id: $('#citySelect').val(),
+                    page: page
+                },
+                url: e.currentTarget.dataset.url,
+                success: (data) => {
+                    this.refreshList(data)
+                },
+            });
+        }
+
+        refreshList(data){
+            document.getElementById('organizationsList').innerHTML = data.html;
+            Pagy.init(document.getElementById('organizationsList'))
         }
     })
 })();
