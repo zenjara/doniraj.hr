83f1df9f98e3f66722a69e278123d9b0296f07e9
Ivan Matas <ivan.matas2@gmail.com>
2020-12-12 15:50:01 +0100

    Implement suggest form, semi styled, few additional changes needed

---
+++ app/assets/javascripts/controllers/suggestion.js
@@ -0,0 +1,77 @@
+(() => {
+    const application = Stimulus.Application.start();
+
+    application.register("suggestion", class extends Stimulus.Controller {
+        static targets = [
+            'formContainer',
+            'form',
+            'formSection',
+            'name',
+            'cityId',
+            'nameError',
+            'cityError'
+        ];
+
+        connect() {
+            $('.suggestionButton').click(function (e) {
+                e.preventDefault();
+                $('#suggestionModal').show();
+                $('#organization_city_id').select2({
+                    placeholder: "Mjesto",
+                    allowClear: true
+                });
+            })
+        }
+
+        closeForm() {
+            $('#suggestionModal').hide();
+        }
+
+        submit(event) {
+            if (!this.validateForm()) {
+                event.preventDefault();
+                this.showError();
+            } else {
+                Rails.fire(this.formTarget, 'submit')
+            }
+        }
+
+        submitSuccess(event) {
+            let [data, status, xhr] = event.detail;
+            if (status === 'Created') {
+                this.formContainerTarget.innerHTML = data.html
+            }
+        }
+
+        validateForm() {
+            this.nameErrorTarget.textContent = ""
+            this.cityErrorTarget.textContent = ""
+
+            if (this.nameTarget.value.trim() === "") {
+                this.nameTarget.focus();
+                this.nameErrorTarget.textContent = "Ime udruge je obavezno polje.";
+                this.targets.findAll("formSection")[0].classList.add("error");
+                return false;
+            }
+
+            if (this.cityIdTarget.value.trim() === "") {
+                this.cityIdTarget.focus();
+                this.cityErrorTarget.textContent = "Molimo izaberite grad.";
+                this.targets.findAll("formSection")[1].classList.add("error");
+                return false;
+            }
+
+            return true;
+        }
+
+        showError() {
+            const errorElements = $(".form__section.error");
+            if (errorElements.length > 0) {
+                $('html, body').animate({
+                    scrollTop: errorElements.first().offset().top - 120
+                }, 200);
+            }
+        }
+
+    })
+})();
--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -10,7 +10,10 @@ class OrganizationsController < ApplicationController
   def create
     @organization = Organization.new(organization_params)
 
-    return render 'thank_you', status: :ok if @organization.save
+    if @organization.save
+      OrganizationMailer.suggestion_email.deliver_later if params[:created_via_suggestion_form]
+      return render json: { html: render_to_string(action: '_thank_you', locals: {}, formats: [:html], layout: false) }, status: :created
+    end
 
     render json: @organization.errors, status: :internal_server_error
   end
@@ -33,6 +36,6 @@ class OrganizationsController < ApplicationController
   private
 
   def organization_params
-    params.require(:organization).permit(:name, :address, :iban, :city_id)
+    params.require(:organization).permit(:name, :address, :description, :iban, :city_id)
   end
 end
