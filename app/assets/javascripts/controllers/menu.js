d18b2f8bbe01a311af9004563beb0342a4116de7
Kristijan Korać <korac.kristijan@gmail.com>
2020-12-13 18:09:19 +0100

    Add mobile form

--- app/assets/javascripts/controllers/menu.js
+++ app/assets/javascripts/controllers/menu.js
@@ -6,7 +6,18 @@
 
         toggleMenu() {
             const mobileMenu = document.getElementById('mobile-menu');
+            const body = document.getElementById('body');
+
             mobileMenu.classList.toggle('mobile-menu--hidden');
+            body.classList.toggle('lock');
+        }
+
+        toggleMobileForm() {
+            const actions = document.getElementById('mobile-menu__actions-wrapper');
+            const form = document.getElementById('mobile-form');
+
+            actions.classList.toggle('none');
+            form.classList.toggle('none');
         }
 
     });
