93333fc424d9ff4cfb00808d5f6616a96ebe8bc9
Kristijan Korać <korac.kristijan@gmail.com>
2020-12-13 18:37:05 +0100

    Add mobile form city select.

--- app/assets/javascripts/controllers/menu.js
+++ app/assets/javascripts/controllers/menu.js
@@ -8,8 +8,17 @@
             const mobileMenu = document.getElementById('mobile-menu');
             const body = document.getElementById('body');
 
+            this.hideMobileForm();
             mobileMenu.classList.toggle('mobile-menu--hidden');
             body.classList.toggle('lock');
+        }
+
+        hideMobileForm() {
+            const actions = document.getElementById('mobile-menu__actions-wrapper');
+            const form = document.getElementById('mobile-form');
+
+            actions.classList.remove('none');
+            form.classList.add('none');
         }
 
         toggleMobileForm() {
@@ -18,6 +27,15 @@
 
             actions.classList.toggle('none');
             form.classList.toggle('none');
+        }
+
+        selectFormCityMobile(event) {
+            const optionElement = document.querySelector(`[value="${event.target.value}"]`);
+            const optionValue = optionElement.textContent;
+
+            const newValueElement = `<span style="color: #111;">${optionValue}</span>`;
+            const placeholder = document.getElementById('mobile-form-placeholder-text');
+            placeholder.innerHTML = newValueElement;
         }
 
     });
