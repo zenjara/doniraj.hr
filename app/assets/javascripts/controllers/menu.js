f5dbaad284900dbc7a915632e86f8091614830a1
Kristijan Korać <korac.kristijan@gmail.com>
2020-12-13 19:50:10 +0100

    Fix mobile suggestion form; Add validation.

--- app/assets/javascripts/controllers/menu.js
+++ app/assets/javascripts/controllers/menu.js
@@ -27,6 +27,13 @@
 
             actions.classList.toggle('none');
             form.classList.toggle('none');
+
+            const formContent = document.getElementById('form-content');
+            formContent.classList.remove('none');
+
+            const successMessage = document.getElementById('mobile-menu__success-message');
+            successMessage.classList.add('none');
+
         }
 
         selectFormCityMobile(event) {
@@ -38,5 +45,54 @@
             placeholder.innerHTML = newValueElement;
         }
 
+        submit(e) {
+            e.preventDefault();
+            const nameInput = document.querySelector('[data-menu-target="name"]');
+            const cityInput = document.querySelector('[data-menu-target="cityId"]');
+
+            const $nameError = $('#mobile-menu__name-error');
+            const $cityError = $('#mobile-menu__city-error');
+
+            if(!nameInput.value) {
+                $nameError.show();
+                return;
+            }
+
+            $nameError.hide();
+
+            if(!cityInput.value) {
+                $cityError.show();
+                return;
+            }
+
+            $cityError.hide();
+
+            const $form = $('#suggestion_form_mobile');
+            const url = $form.attr('action');
+            const data = $form.serialize();
+
+            $.ajax({
+                cache: false,
+                type: 'post',
+                data: data,
+                url: url,
+                success: this.handleSuccess
+            });
+        }
+
+        handleSuccess() {
+            document.getElementById('suggestion_form_mobile').reset();
+            const newValueElement = `<span style="color: #111;">Mjesto</span>`;
+            const placeholder = document.getElementById('mobile-form-placeholder-text');
+            placeholder.innerHTML = newValueElement;
+
+            const formContent = document.getElementById('form-content');
+            formContent.classList.add('none');
+
+            const successMessage = document.getElementById('mobile-menu__success-message');
+            successMessage.classList.remove('none');
+        }
+
+        handleError() {}
     });
 })();
